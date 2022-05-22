edce4a1d52bb2ea8805763a09e647a9a23c9e0e3
Nathan <30803215+Nathan903@users.noreply.github.com>
2022-05-22 13:21:34 +0800

    Update download_and_clean_antiAD.py

--- download_and_clean_antiAD.py
+++ download_and_clean_antiAD.py
@@ -9,6 +9,7 @@ with urllib.request.urlopen(src) as data:
         line=line.decode("utf-8")
         if bannedinitials in line:
             #print(line)
+            
         else:
             result+=line
 with open("anti-ad-easylist-cleaned.txt", "w", encoding="utf-8") as f:
